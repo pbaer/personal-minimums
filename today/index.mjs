94ecde1a357bb06d310544f6edc311d3d06efb13
Peter Baer <pbaer@microsoft.com>
2022-11-06 17:00:17 -0800

    Fix

--- today/index.mjs
+++ today/index.mjs
@@ -5,8 +5,8 @@ export default async function (context, req) {
     try {
         const name = (req.query.name || (req.body && req.body.name));
         const responseMessage = name
-            ? "Hello, " + name + ". This HTTP triggered function executed successfully!!!!" + test()
-            : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response!!!" + test();
+            ? "Hello, " + name + ". This HTTP triggered function executed successfully!!!!" + await test()
+            : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response!!!" + await test();
     
         context.res = {
             // status: 200, /* Defaults to 200 */
