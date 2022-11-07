f75273067010aa944d72433ae81245f81a0249dc
Peter Baer <pbaer@microsoft.com>
2022-11-06 16:12:39 -0800

    Read from KeyVault

--- src/index.mjs
+++ src/index.mjs
@@ -1,3 +1,9 @@
-export const test = () => {
-    return 'abc';
+import { SecretClient } from '@azure/keyvault-secrets';
+import { DefaultAzureCredential } from '@azure/identity';
+
+export const test = async () => {
+    const credential = new DefaultAzureCredential();
+    const client = new SecretClient('https://pbaer.vault.azure.net', credential);
+    const secret = await client.getSecret('metar-taf-api');
+    return `secret is ${secret ? secret.length : 'no-secret'} chars long, value is ${(secret && secret.value) ? secret.value.length : 'no-value'} chars long`;
 };
--- today/index.mjs
+++ today/index.mjs
@@ -2,14 +2,17 @@ import { test } from '../src/index.mjs';
 
 export default async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
-
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully!!!!" + test()
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response!!!" + test();
-
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+    try {
+        const name = (req.query.name || (req.body && req.body.name));
+        const responseMessage = name
+            ? "Hello, " + name + ". This HTTP triggered function executed successfully!!!!" + test()
+            : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response!!!" + test();
+    
+        context.res = {
+            // status: 200, /* Defaults to 200 */
+            body: responseMessage
+        };    
+    } catch (err) {
+        context.log(`Error: ${err}`);
+    }
 }
