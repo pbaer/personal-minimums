ed5f201b20497c27ffde375be9069ab9aa90a8c4
Peter Baer <pbaer@microsoft.com>
2022-11-21 18:54:13 -0800

    Fix

--- today/index.mjs
+++ today/index.mjs
@@ -1,11 +1,12 @@
 import { printToday } from '../src/index.mjs';
 
-export default async function (context, _req) {
+export default async function (context, req) {
     context.log('Starting execution');
     try {
+        const utcOffset = req.query && req.query.utcOffset;
         context.res = {
             status: 200,
-            body: await printToday(req.query.utcOffset)
+            body: await printToday(utcOffset)
         };    
     } catch (err) {
         context.res = {
